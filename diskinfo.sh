4fec62137df5bc9d1fea3aabce44910beef848cb
miner <miner@example.com>
2021-05-05 12:19:42 +0000

    diskinfo

---
+++ diskinfo.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+#
+echo "nvme io_poll (1=polling on),io_poll_delay(1 = always poll, 0 = hybrid mode)"
+
+files=$(ls /dev/)
+for device in $files
+do
+ #echo $device
+ if [[ $device == nvme*n* ]]; then
+	echo "======$device======"
+    cmd1="/sys/block/$device/queue/io_poll"
+    cmd2="/sys/block/$device/queue/io_poll_delay "
+  	echo "$device" io_pool:$(cat $cmd1), io_poll_delay:$(cat $cmd2)
+	echo $(lsblk | grep $device)
+  fi
+done
+
