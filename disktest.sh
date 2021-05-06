82833b58690d96fa80fd13a24cca070eed812c1f
miner <miner@example.com>
2021-05-06 02:34:04 +0000

    driver test args

--- disktest.sh
+++ disktest.sh
@@ -1,14 +1,18 @@
 #! /bin/bash
-  
+
+if [ $# -eq 0 ]; then
+    echo "No disk device provided"
+    exit 1
+fi
+
+apt-get install fio
+
 SEQIODEPTH=8
 SEQIOSIZE=1m
 SEQJOBS=10
 RUNTIME=60
 
-fio -filename=/dev/sdb -direct=1 -iodepth=$SEQIODEPTH -thread -rw=read -ioengine=libaio -bs=$SEQIOSIZE -numjobs=$SEQJOBS -name="SSD Seq Read Test" -group_reporting -runtime=$RUNTIME
+fio -filename=/dev/$1 -direct=1 -iodepth=$SEQIODEPTH -thread -rw=read -ioengine=libaio -bs=$SEQIOSIZE -numjobs=$SEQJOBS -name="SSD Seq Read Test" -group_reporting -runtime=$RUNTIME
 
-fio -filename=/dev/sdb -direct=1 -iodepth=$SEQIODEPTH -thread -rw=write -ioengine=libaio -bs=$SEQIOSIZE -numjobs=$SEQJOBS -name="SD Seq Write Test" -group_reporting -runtime=$RUNTIME
+fio -filename=/dev/$1 -direct=1 -iodepth=$SEQIODEPTH -thread -rw=write -ioengine=libaio -bs=$SEQIOSIZE -numjobs=$SEQJOBS -name="SD Seq Write Test" -group_reporting -runtime=$RUNTIME
 
-#fio -filename=/dev/md0 -direct=1 -iodepth=$SEQIODEPTH -thread -rw=read -ioengine=libaio -bs=$SEQIOSIZE -numjobs=$SEQJOBS -name="SSD Seq Read Test" -group_reporting -runtime=$RUNTIME
-
-#fio -filename=/dev/md0 -direct=1 -iodepth=$SEQIODEPTH -thread -rw=write -ioengine=libaio -bs=$SEQIOSIZE -numjobs=$SEQJOBS -name="SD Seq Write Test" -group_reporting -runtime=$RUNTIME
