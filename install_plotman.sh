22bd6e24512a802a1b01ef985f3ca9d06f4661fa
miner <miner@example.com>
2021-05-07 09:48:14 +0000

    git proxy

--- install_plotman.sh
+++ install_plotman.sh
@@ -11,11 +11,12 @@ else
     ###  Control will jump here if $DIR does NOT exists ###
     echo "****** Install plotman ******"
     cd /opt/chia
+	git config --global http.proxy http://access.futsystems.com:8888
     git clone https://github.com/ericaltendorf/plotman.git
     . /opt/chia/chia-blockchain/venv/bin/activate
     cd /opt/chia/plotman
     python setup.py install
-    
+    git config --global --unset http.proxy
 	# install supervisor conf
 	ln -s /opt/src/supervisor/plot.conf /etc/supervisor/conf.d/plot.conf 
   else
