d819bc421747a3dc9322e2bee23e942468024421
qianbo <qianbo@futsystems.com>
2021-05-07 15:50:32 +0000

    add pool

---
+++ install_hpoll.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+mkdir -p /opt/hpool
+cd /opt/hpool
+
+cp -rf /opt/src/hpool/hpool-miner-chia /opt/hpool
+cp -rf /opt/src/hpool/config.yaml /opt/hpool
+
+ln -s /opt/src/supervisor/hpool.conf /etc/supervisor/conf.d/hpool.conf
+
+echo please change miner Name in config
+
+
+
+
+
+
+
+
