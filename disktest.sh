dfb23ad8491592b5d9230209cf4d46bd5eef198b
miner <miner@example.com>
2021-05-13 07:04:14 +0000

    disktest

--- disktest.sh
+++ disktest.sh
@@ -11,8 +11,10 @@ SEQIODEPTH=8
 SEQIOSIZE=1m
 SEQJOBS=10
 RUNTIME=60
+FILESIZE=1G
 
-fio -filename=/dev/$1 -direct=1 -iodepth=$SEQIODEPTH -thread -rw=read -ioengine=libaio -bs=$SEQIOSIZE -numjobs=$SEQJOBS -name="SSD Seq Read Test" -group_reporting -runtime=$RUNTIME
+fio -filename=$1/test.img -size=$FILESIZE -direct=1 -iodepth=$SEQIODEPTH -thread -rw=read -ioengine=libaio -bs=$SEQIOSIZE -numjobs=$SEQJOBS -name="SSD Seq Read Test" -group_reporting -runtime=$RUNTIME
 
-fio -filename=/dev/$1 -direct=1 -iodepth=$SEQIODEPTH -thread -rw=write -ioengine=libaio -bs=$SEQIOSIZE -numjobs=$SEQJOBS -name="SD Seq Write Test" -group_reporting -runtime=$RUNTIME
+fio -filename=$1/test.img -size=$FILESIZE -direct=1 -iodepth=$SEQIODEPTH -thread -rw=write -ioengine=libaio -bs=$SEQIOSIZE -numjobs=$SEQJOBS -name="SD Seq Write Test" -group_reporting -runtime=$RUNTIME
 
+rm -rf $1/test.img
