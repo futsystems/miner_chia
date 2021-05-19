b8cbe68a671d9ea51856766a2526465c40e58882
miner <miner@example.com>
2021-05-19 08:51:36 +0000

    mv nas to harvester

--- install_api_harvester.sh
+++ install_api_harvester.sh
@@ -6,8 +6,8 @@ apt install python3-virtualenv
 apt install pv
 
 #mkdir
-mkdir -p /opt/nas
-mkdir -p /opt/nas/config
+mkdir -p /opt/harvester
+mkdir -p /opt/harvester/config
 
 cd /opt/nas
 git clone https://gitlab.marvelsystem.net/miner/plotmgr.git bin
@@ -17,8 +17,8 @@ virtualenv venv
 source ./venv/bin/activate
 pip install -r requirement.txt
 
-ln -s /opt/nas/bin/supervisor/nas.conf /etc/supervisor/conf.d/nas.conf
-cp plotmgr.conf /opt/nas/config/
+ln -s /opt/harvester/bin/supervisor/nas.conf /etc/supervisor/conf.d/nas.conf
+cp plotmgr.conf /opt/harvester/config/
 
 
 
