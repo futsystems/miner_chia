9b41bc10b38bc35481349a8a822a696804ef391e
miner <miner@example.com>
2021-06-16 08:18:44 +0000

    fast plotter

---
+++ scripts/fast_plotter.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/opt/src/plotter/chia_plot -n 1 -r 16 -t /mnt/cache/00/ -d /mnt/cache/00/dst/ -p af611c57169eb54c19563ad2208c6b44ea364d3cd8b1eea2f8ed5706196f4636e1a7fa91001c244f1308cabb10ebf364 -f b9ef776c016c9fb52dd6b100b88af21e515c9e61cd3aa397b3c15a325207d9206a3bcbedb4acbfa37aa378117adf00cc
