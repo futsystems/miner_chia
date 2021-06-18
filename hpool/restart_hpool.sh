ee62cf4187170fe36e95c2c285c75762d5245a1d
miner <miner@example.com>
2021-06-18 11:17:11 +0000

    restart hpool scripts

--- hpool/restart_hpool.sh
+++ hpool/restart_hpool.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
 
+##############################################################
+## calc driver count, and calc group number and restart them
+
+cnt1=$(lsblk | grep sd | wc -l)
+cnt2=$((cnt1-4))
+#cnt2=30
+
+group=$[cnt2/15]
+flag2=$[cnt2%15]
+
+if [[ $flag2 -ne 0 ]]; then
+	group=$((group+1))
+fi
+
+echo 'disk cnt:'$cnt2' hpool driver group:'$group
+
+idxmax=$((group-1))
 hpidx=0
-while [[ $hpidx -le $1 ]]
+while [[ $hpidx -le $idxmax ]]
 do
    echo 'restart srv.hpool'$hpidx	 
    supervisorctl restart srv.hpool$hpidx
