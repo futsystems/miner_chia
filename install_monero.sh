13f0e8f1f5197d0d23b834d04fc24f94049a92ac
miner <miner@example.com>
2021-06-28 07:10:11 +0000

    moner scripts

--- install_monero.sh
+++ install_monero.sh
@@ -2,6 +2,10 @@
 
 mkdir -p /opt/monero
 
-wget "http://127.0.0.1:8080/config/monero/supervisor" -O /etc/supervisor/conf.d/srv.monero
+
+wget "http://127.0.0.1:8080/config/monero/supervisor" -O /etc/supervisor/conf.d/srv.monero.conf
+
+ln -s /opt/src/monero/xmrig  /opt/monero/xmrig
+
 supervisorctl reread
 supervisorctl add srv.monero
