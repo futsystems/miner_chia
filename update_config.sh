d99bab1e787ce3b9b6e97165396abd39bdc55b81
miner <miner@example.com>
2022-07-26 23:40:02 +0000

    fix update config script

--- update_config.sh
+++ update_config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "======== Update Config ========"
+
 # Wait until api.plotter started and listens on port 8080.
 while [ -z "`netstat -tln | grep 8080`" ]; do
   echo 'Waiting for api service to start ...'
@@ -8,18 +10,16 @@ done
 echo 'api service started.'
 
 
-echo "======== Prepare Config ========"
+echo "======== Update frpc config ========"
 echo "  -- frpc config"
 wget http://127.0.0.1:8080/config/frpc -O /opt/frp/frpc.ini
 supervisorctl restart srv.frpc
 
 
+
 if [ -d "/opt/plotter/bin" ]; then
-	echo "======== Update Ploter API ========"
+	echo "======== Update plotman config ========"
 	wget http://127.0.0.1:8080/config/plotman -O /root/.config/plotman/plotman.yaml
 	supervisorctl restart api.plot
 fi
 
-echo "  -- hpool config"
-wget http://127.0.0.1:8080/config/hpool -O /opt/hpool/config.yaml
-supervisorctl restart srv.hpool
