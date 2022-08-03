3457e8d9348116d0eba568c12a6859fc20e22eea
miner <miner@example.com>
2022-08-03 08:31:32 +0000

    upgrade hpool

--- install_hpool.sh
+++ install_hpool.sh
@@ -4,11 +4,11 @@ mkdir -p /opt/hpool
 cd /opt/hpool
 
 cp -rf /opt/src/hpool/hpool-miner-chia /opt/hpool
-cp -rf /opt/src/hpool/config.yaml /opt/hpool
+#cp -rf /opt/src/hpool/config.yaml /opt/hpool
 
-ln -s /opt/src/supervisor/hpool.conf /etc/supervisor/conf.d/hpool.conf
+#ln -s /opt/src/supervisor/hpool.conf /etc/supervisor/conf.d/hpool.conf
 
-echo please change miner Name in config
+#echo please change miner Name in config
 
 
 
