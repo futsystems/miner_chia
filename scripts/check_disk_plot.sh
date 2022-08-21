9fd9a4f11c7711539942cf67932ea92fc49dd49c
miner <miner@example.com>
2022-08-21 02:59:39 +0000

    add check scripts

--- scripts/check_disk_plot.sh
+++ scripts/check_disk_plot.sh
@@ -1,3 +1,38 @@
 #!/bin/sh
-fstrim -v /mnt/cache/00
-fstrim -v /mnt/cache/01
+
+if [ $# -eq 0 ]; then
+	echo "please privider hpool index"
+	exit 1
+fi
+
+
+hpidx=$1
+
+tmp1=$((hpidx+1))
+
+driveridx=`expr $hpidx \* 15`
+driverend=`expr $tmp1 \* 15 - 1`
+
+
+echo 'hpool idx:'$hpidx' driver start: '$driveridx' end: '$driverend
+
+
+
+while [ $driveridx -le $driverend ]
+do
+
+	cd /mnt/plots/driver$driveridx
+	find . -name "*" -type f -size -101G | xargs -n 1 rm -f
+	
+	fcnt=$(ls /mnt/plots/driver$driveridx -l | grep "^-" | wc -l)
+
+	ss=$(df )
+
+	
+	echo '/mnt/plots/driver'$driveridx' plots cnt: '$fcnt
+
+	driveridx=$((driveridx+1))
+
+done
+
+
